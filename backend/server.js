8fc77ac665c05df68263a4eefb6f34a305504461
Mahesh <maheshdsu86@gmail.com>
2023-06-29 21:41:53 +0530

    sas

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,13 @@ app.use('/',auth)
 //----- deployement code -----//
 
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("frontend/build"));
-    const path = require("path")
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-    })
-}
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("frontend/build"));
+//     const path = require("path")
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+//     })
+// }
 
 
 //----- server listen -----//
